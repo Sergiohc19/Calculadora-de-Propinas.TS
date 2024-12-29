0b958411e7cd97ab71ea72f14305b329ae46d381
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2024-12-29 03:25:57 +0100

    Estilos en App.tsx y MenuItem.tsx
    Implementando estilos en los componentes

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,7 +1,14 @@
+import type { MenuItem as MenuItemType } from "../types";
 
+type MenuItemProps = {
+    item: MenuItemType; // Aquí usas el tipo renombrado
+};
 
-export const MenuItem = () => {
-  return (
-    <div>MenuItem</div>
-  )
-}
+export const MenuItem = ({ item }: MenuItemProps) => {
+    return (
+        <button className="rounded-xl border-2 border-teal-400 w-full p-3 hover:bg-teal-300  flex justify-between">
+            <p>{item.name}</p>
+            <p className="font-black">${item.price}</p>
+        </button>
+    );
+};
