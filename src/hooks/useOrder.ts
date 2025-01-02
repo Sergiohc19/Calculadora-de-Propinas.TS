81acdb6febde2b40d4546c011b964cabb799558c
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-01-02 18:21:53 +0100

    Evitar duplicados
    Evitar duplicados de productos

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -6,14 +6,22 @@ export const UseOrder = () => {
 
   const [order, setOrder] = useState<OrderItem[]>([])
 
-  const addItem = (item : MenuItem) => {
-    
-    const newItem : OrderItem = {...item, quantity: 1}
-    setOrder([...order, newItem])
+  const addItem = (item: MenuItem) => {
 
+    const itemExist = order.find(orderItem => orderItem.id === item.id)
+    if (itemExist) {
+     const updateOrder =  order.map( orderItem => orderItem.id === item.id ? {...orderItem, quantity : 
+      orderItem.quantity + 1} : 
+      orderItem )
+      setOrder(updateOrder)
+
+    } else {
+      const newItem: OrderItem = { ...item, quantity: 1 }
+      setOrder([...order, newItem])
+    }
   }
 
-    console.log(order)
+  console.log(order)
   return {
     addItem
   }
