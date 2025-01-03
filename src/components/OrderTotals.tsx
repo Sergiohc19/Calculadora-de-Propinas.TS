595679aeefa73e9ab2752e9c2028e6202fe0ea0e
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-01-03 12:16:40 +0100

    Calculando el total a pagar con  useMemo

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -14,6 +14,7 @@ export default function OrderTotals({order, tip} : OrderTotalsProps) {
 
   const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
   const tipAmount = useMemo(() => subtotalAmount * tip,  [tip, order])
+  const totalAmount = useMemo(() => subtotalAmount + tipAmount,  [tip, order])
 
 
   return (
@@ -32,7 +33,7 @@ export default function OrderTotals({order, tip} : OrderTotalsProps) {
 
 </p>
 <p>Total a pagar:
-<span className="font-bold"> $0</span>
+<span className="font-bold"> {formatCurrency (totalAmount)}</span>
 
 
 </p>
--- src/components/TipPercentageForm.tsx
+++ src/components/TipPercentageForm.tsx
@@ -2,25 +2,35 @@ import type { Dispatch, SetStateAction } from "react";
 
 const tipOptions = [
   {
+    id: "tip-05",
+    value: 0.05,
+    label: "5%",
+  },
+  {
     id: "tip-10",
-    value: 0.1,
+    value: 0.10,
     label: "10%",
   },
   {
+    id: "tip-15",
+    value: 0.15,
+    label: "15%",
+  },
+  {
     id: "tip-20",
-    value: 0.2,
+    value: 0.20,
     label: "20%",
   },
   {
-    id: "tip-50",
-    value: 0.5,
-    label: "50%",
+    id: "tip-25",
+    value: 0.25,
+    label: "25%",
   },
 ];
 
 type TipPercentageFormProps = {
     setTip: React.Dispatch<React.SetStateAction<number>>
-    
+
 
     
 }
